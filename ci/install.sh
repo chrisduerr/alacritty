fd20f80d8f1d11d33907a8c8ccb317d67f74395b
Christian Duerr <contact@christianduerr.com>
2018-10-26 23:43:53 +0200

    Remove choco from install script

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Install choco for windows builds
-if [ "$TRAVIS_OS_NAME" == "windows" ]; then
-    choco install llvm --norestart --nosilent || exit 1
-fi
-
 # Add clippy for linting with nightly builds
 if [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
     rustup component add clippy-preview
