089bdc0fa6efb4582ad57d21cc311b912c7cb486
Christian Duerr <contact@christianduerr.com>
2018-10-27 02:47:23 +0200

    Fix auxillary files

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -11,6 +11,7 @@ aux_files=("alacritty-completions.bash"
            "alacritty_windows.yml")
 
 # Get previous tag to check for changes
+git fetch --tags
 prev_tag=$(git describe --tags --abbrev=0 $TRAVIS_TAG^)
 
 # Everything in this directory will be offered as download for the release
